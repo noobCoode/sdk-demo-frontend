eb554e8517f81c09ebdc8b5e9331061ee0808d6f
xudongdong <1097374154@qq.com>
2020-05-19 21:29:59 +0800

    feat:实现锁功能

--- src/components/EditFile.vue
+++ src/components/EditFile.vue
@@ -7,7 +7,8 @@
         :autosize="{ minRows: 2, maxRows: 4}"
         placeholder="请输入文件内容"
         v-model="content"
-        @input="sendMsg"
+        @input="sendMessage"
+        :disabled="!canWrite"
       >
       </el-input>
       <el-button
@@ -25,10 +26,7 @@
 </template>
 
 <script>
-  let stompClient = null
-  import SockJs from 'sockjs-client'
-  import Stomp from 'stompjs'
-
+  let websocket = null
   export default {
     name: "EditFile.vue",
     data() {
@@ -40,7 +38,7 @@
     },
     created() {
       this.initHttp()
-      this.initWebSocket(stompClient)
+      this.establishWebsocket()
 
     },
     methods: {
@@ -55,23 +53,33 @@
         })
       },
 
-      initWebSocket() {
-        let socket = new SockJs(`http://localhost:8081/socket`)
-        stompClient = Stomp.over(socket)
-        stompClient.connect({}, (frame) => {
-          stompClient.subscribe(`/topic/files/${this.title}`, (message) => {
-            console.log("message=" + message)
-            this.content = JSON.parse(message.body).message
-            this.canWrite = JSON.parse(message.body).canWrite
-          })
-          stompClient.subscribe(`/topic/files/${this.title}/who`, (message) => {
-            alert(message)
-          })
-        })
+      establishWebsocket() {
+        if (websocket == null) {
+          websocket = new WebSocket(`ws://localhost:8081/websocket/${this.getLastPathInUrl(window.location.href)}/${getCookie("Session_id")}`)
+          websocket.onopen = () => {
+            console.log("已连接")
+          }
+          websocket.onmessage = (msg) => {
+            let msgInfo=eval(`(${msg.data})`)
+            console.log(msgInfo)
+            //如果是锁消息
+            if (msgInfo.lockMsg) {
+              console.log(msgInfo.jSessionId === getCookie("Session_id"))
+              this.canWrite = msgInfo.jSessionId === getCookie("Session_id")
+            } else {
+              this.content =msgInfo.msg
+            }
+          }
+        }
       },
 
-      sendMsg() {
-        stompClient.send(`/app/files/${this.title}`, {}, this.content)
+      sendMessage() {
+        let msgInfo={
+          "jSessionId":getCookie("Session_id"),
+          "msg":this.content,
+          "isLockMsg":false
+        }
+        websocket.send(JSON.stringify(msgInfo))
       },
 
       submit() {
@@ -98,6 +106,30 @@
       }
     },
 
+  }
+
+
+  function getCookie(cookie_name) {
+    var allcookies = document.cookie;
+    //索引长度，开始索引的位置
+    var cookie_pos = allcookies.indexOf(cookie_name);
+
+    // 如果找到了索引，就代表cookie存在,否则不存在
+    if (cookie_pos !== -1) {
+      // 把cookie_pos放在值的开始，只要给值加1即可
+      //计算取cookie值得开始索引，加的1为“=”
+      cookie_pos = cookie_pos + cookie_name.length + 1;
+      //计算取cookie值得结束索引
+      var cookie_end = allcookies.indexOf(";", cookie_pos);
+
+      if (cookie_end === -1) {
+        cookie_end = allcookies.length;
+
+      }
+      //得到想要的cookie的值
+      var value = unescape(allcookies.substring(cookie_pos, cookie_end));
+    }
+    return value;
   }
 </script>
 
